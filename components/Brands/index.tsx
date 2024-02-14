3222b193d4c300b47ccf9c5a11daaa2f10fffc02
Austin Hunter <ahunter8850@outlook.com>
2024-02-14 09:28:36 -0600

    images

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -29,17 +29,16 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
   const { href, image, name } = brand;
 
   return (
-    <div className="mx-3 flex w-full max-w-[160px] items-center justify-center py-[15px] sm:mx-4 lg:max-w-[130px] xl:mx-6 xl:max-w-[150px] 2xl:mx-8 2xl:max-w-[160px]">
+    <div className="mx-3 flex w-full max-w-[100px] items-center justify-center py-[15px] sm:mx-4 lg:max-w-[100px] xl:mx-6 xl:max-w-[130px] 2xl:mx-8 2xl:max-w-[150px]">
       <a
         href={href}
         target="_blank"
         rel="nofollow noreferrer"
         className="relative h-10 w-full opacity-70 grayscale transition hover:opacity-100 hover:grayscale-0 dark:opacity-60 dark:hover:opacity-100"
       >
-        {
-        //<Image src={image} alt={name} fill />
-        }
-        <p className="pr-[10px] text-base font-medium leading-relaxed text-body-color" style={{fontSize: '16px', textAlign: 'center'}}>{name}</p>
+        
+        <Image src={image} alt={name} fill />
+        
       </a>
     </div>
   );
