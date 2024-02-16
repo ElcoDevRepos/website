a954968bbcf232a012a3eed25945fce2c1cda822
Austin Hunter <ahunter8850@outlook.com>
2024-02-16 10:14:37 -0600

    update web

--- components/Pricing/PricingBox.tsx
+++ components/Pricing/PricingBox.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const PricingBox = (props: {
   price: string;
   duration: string;
@@ -24,9 +26,12 @@ const PricingBox = (props: {
         </div>
         <p className="mb-7 text-base text-body-color">{subtitle}</p>
         <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
-          <button className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
-            Contact Us
-          </button>
+        <Link
+                    href="/contact"
+                    className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                  >
+                    Contact 
+                  </Link>
         </div>
         <div>{children}</div>
         <div className="absolute bottom-0 right-0 z-[-1]">
