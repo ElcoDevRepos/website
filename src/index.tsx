e67bfc3a04ddbcbae28a50d1835492ee0fa147cf
Austin Hunter <ahunter8850@outlook.com>
2025-01-06 11:03:30 -0600

    analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Analytics } from "@vercel/analytics/react"
+import { initGA } from './utils/analytics';
+
+// Initialize Google Analytics
+initGA();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
