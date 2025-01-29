f54c8cce6a83cce8eb0b3eeea1d0b4ac1a704c63
Austin Hunter <ahunter8850@outlook.com>
2025-01-29 15:12:45 -0600

    test

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -2,6 +2,7 @@ import ReactGA from 'react-ga4';
 
 export const initGA = () => {
   ReactGA.initialize('G-NZY90G4L7Y');
+  ReactGA.initialize('G-HMTR3GHTDW');
 };
 
 export const logPageView = () => {
