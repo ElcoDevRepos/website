95709a2e0a4c49c7254f34c653adee0aaf6c550e
Austin Hunter <ahunter8850@outlook.com>
2025-03-12 22:20:06 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect } from 'react';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
@@ -18,56 +17,13 @@ import PartnersPage from './components/Partners/PartnersPage';
 // import BlogPage from './components/Blog/BlogPage';
 // import BlogPost from './components/Blog/BlogPost';
 // import BlogGenerator from './components/Blog/BlogGenerator';
+import FAITracker from "./FAITracker";
 
 const App: React.FC = () => {
 
-useEffect(() => {
-    // Check if the script is already loaded
-    if (document.getElementById("faitracker-script")) return;
-
-    // Create the tracking script
-    const trackerScript = document.createElement("script");
-    trackerScript.innerHTML = `
-      window.faitracker=window.faitracker||function(){
-        this.q=[];var t=new CustomEvent("FAITRACKER_QUEUED_EVENT");
-        return this.init=function(t,e,a){
-          this.TOKEN=t,this.INIT_PARAMS=e,this.INIT_CALLBACK=a,
-          window.dispatchEvent(new CustomEvent("FAITRACKER_INIT_EVENT"))
-        },
-        this.call=function(){
-          var e={k:"",a:[]};
-          if(arguments&&arguments.length>=1){
-            for(var a=1;a<arguments.length;a++)e.a.push(arguments[a]);
-            e.k=arguments[0]
-          }
-          this.q.push(e),window.dispatchEvent(t)
-        },
-        this.message=function(){
-          window.addEventListener("message",function(t){
-            "faitracker"===t.data.origin&&this.call("message",t.data.type,t.data.message)
-          })
-        },
-        this.message(),
-        this.init("to2lxri0d2wnl458ign6jw0gjraya6dv",{host:"https://api.factors.ai"}),
-        this
-      }();
-    `;
-    document.body.appendChild(trackerScript);
-
-    // Load external Factors.ai script
-    const script = document.createElement("script");
-    script.src = "https://app.factors.ai/assets/factors.js";
-    script.async = true;
-    script.id = "faitracker-script";
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <Router>
+<FAITracker />
       <Routes>
         {/* Redirect /our-team to home */}
         <Route path="/our-team" element={<Navigate to="/" replace />} />
